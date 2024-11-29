f9864f16843c388262239003ae7190c420ddec91
Scott McPeak <scott.g.mcpeak@gmail.com>
2024-11-29 12:10:02 -0800

    data.lua: Use a slightly smaller (48x48) sprite for the indicator.

--- data.lua
+++ data.lua
@@ -3,13 +3,19 @@
 
 
 -- Indicator that appears when repair packs are hidden.
+--
+-- It is the same image as the thumbnail image for the mod, but scaled
+-- down just a bit from 64x64, which I thought was obnoxiously large
+-- onscreen, to 48x48.  I also tried 32x32 but that seemed too small.
+--
 local indicator_sprite = {
   type = "sprite",
   name = "hide-repair-packs-no-repair-indicator",
-  filename = "__HideRepairPacks__/thumbnail.png",
-  width = 64,
-  height = 64,
+  filename = "__HideRepairPacks__/graphics/sprites/indicator.png",
+  width = 48,
+  height = 48,
 };
+
 
 data:extend{
   indicator_sprite,
